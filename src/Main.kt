5ce7586c8eb18e818869a7a583be5a468f22947f
Vladislav <zv97@mail.ru>
2020-02-06 18:30:49 +0600

    add task1()

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main(args : Array<String>) {
-    
+    task1()
+    task2()
+}
+
+fun task1() {
+    val minutes = readLine()?.toInt()?:return
+    var hours: Int
+    val days: Int
+
+    hours = minutes / 60 + if (minutes%60 >= 30) 1 else 0
+    days = hours / 24
+    hours -= days*24
+    print("days: $days\nhours: $hours")
 }

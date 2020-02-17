2612c6606884d43962597e5de54b73d4cbc6ffc8
Vladislav <zv97@mail.ru>
2020-02-17 14:18:19 +0600

    add task1 subsets set

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
-fun main(args : Array<String>) {
-    
+fun <T> Collection<T>.task1(): Set<Set<T>> {
+    val a: MutableSet<Set<T>> = mutableSetOf(this.toSet())
+    this.map{a.addAll(this.minus(it).toSet().task1())}
+    return a.sortedWith(compareBy{it.size}).toSet()
 }
+
+fun <T> Collection<T>.task1_2(): Set<Set<T>> = this.asSequence()
+.fold(setOf(emptySet())) {
+        acc,
+        v -> acc.union(acc.map {
+            st -> st.union(setOf(v))
+        }.toSet())
+}
+
+fun callTask1() {
+    var myset = readLine()!!.split(' ').map { it.toInt() }.toSet().task1()
+    println(myset.size)
+    println(myset.joinToString("\n"){it.joinToString(" ")})
+}
+
+    fun main(args: Array<String>) {
+        callTask1()
+    }

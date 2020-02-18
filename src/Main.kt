fc27350dc63786f3c956cfeec51d546ce719df84
Vladislav <zv97@mail.ru>
2020-02-18 17:45:59 +0600

    add task 3, number splitting on sum

--- src/Main.kt
+++ src/Main.kt
@@ -18,6 +18,44 @@ fun callTask1() {
     println(myset.joinToString("\n"){it.joinToString(" ")})
 }
 
+fun <T> Collection<T>.task2(k: Int): Set<Set<T>> {
+    val a: MutableSet<Set<T>> = mutableSetOf(this.toSet())
+    this.map { a.addAll(this.minus(it).toSet().task2(k)) }
+    return a.filter { it.size == k }.toSet().sortedWith(compareBy { it.size }).toSet()
+}
+
+fun <T> Collection<T>.task2_1(k: Int): Set<Set<T>>  = this.asSequence()
+        .fold(setOf<Set<T>>(emptySet())) { acc,
+                                          v ->
+            acc.union(acc.map { st ->
+                st.union(setOf(v))
+            }.toSet())
+        }.filter { it.size == k }.toSet()
+
+
+fun callTask2() {
+    val inputSet = readLine()!!.split(' ').map { it.toInt() }.toSet()
+    val k = readLine()!!.toInt()
+    val myset = inputSet.task2(k)
+    println(myset.size)
+    println(myset.joinToString("\n"){ it.joinToString(" ") })
+}
+
+fun sums(n1:Int, n: Int): Set<List<Int>> = List(n) {i-> i+1}.fold(setOf(emptyList())){
+        acc, v->
+    acc.union(sums(n1, n-v).map{it-> it.plusElement(v).sorted()}).filter {
+        it.sum() == n && !it.contains(n1)}.toSet()
+}
+
+fun callTask3() {
+    val n = readLine()!!.toInt()
+    val answer = sums(n,n)
+    println(answer.size)
+    println(answer.joinToString("\n"){it.joinToString(" + ")})
+}
+
     fun main(args: Array<String>) {
-        callTask1()
+//        callTask1()
+//        callTask2()
+        callTask3()
     }
